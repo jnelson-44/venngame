7de016bee07f98c40b76fbe1eecabbfaf1b217af
Jim DeLois <jimdelois@users.noreply.github.com>
2026-04-19 11:35:56 -0400

    Updating Failing Tests with Dictionary Mock

--- tests/unit/lib/Puzzle/test_Puzzle.py
+++ tests/unit/lib/Puzzle/test_Puzzle.py
@@ -1,16 +1,30 @@
 import unittest
+from unittest.mock import patch, AsyncMock
 
 from src.lib.Puzzle.Criteria import Common, Length
 from src.lib.Puzzle import *
 
-class TestPuzzle(unittest.TestCase):
+class TestPuzzle(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
+        # Set up generic Puzzle
         self.criteria = [
             Common.EndsWithLetter("r"),
             Length.AtLeastLength(8),
             Common.HasDoubleLetters()
         ]
         self.puzzle = Puzzle("test-puzz", self.criteria)
+
+        # Set up Dictionary mocking
+        self.word_exists_patcher = patch(
+            "src.lib.Puzzle.Dictionary.word_exists",
+            new_callable=AsyncMock
+        )
+        self.mock_word_exists = self.word_exists_patcher.start()
+        # Allow all words for the test cases in this file
+        self.mock_word_exists.return_value = True
+
+    def tearDown(self):
+        self.word_exists_patcher.stop()
 
     def test_get_name(self):
         """Primitive test case: Get the name of the Puzzle"""
@@ -36,25 +50,25 @@ class TestPuzzle(unittest.TestCase):
                 self.assertEqual(res[0], case["expected_region"], msg=f"Region ID test failed for Region {case["expected_region"]} using word {case["word"]}")
                 self.assertEqual(res[1], case["expected_criteria_matches"], msg=f"Criteria test failed for Region {case["expected_region"]} using word {case["word"]}")
 
-    def test_successful_solve(self):
+    async def test_successful_solve(self):
         """A valid submission is marked as True"""
-        result = self.puzzle.solve_with(["tester", "something", "rabbit", "reservoir", "rebuttal", "butter", "abattoir"])
+        result = await self.puzzle.solve_with(["tester", "something", "rabbit", "reservoir", "rebuttal", "butter", "abattoir"])
         self.assertTrue(result)
 
-    def test_solution_failure_too_few(self):
+    async def test_solution_failure_too_few(self):
         """A ValueError is raised if there are too few submissions"""
         with self.assertRaises(ValueError):
             # The configured test puzzle requires 7 regions; providing six
-            self.puzzle.solve_with(["one", "two", "three", "four", "five", "six"])
+            await self.puzzle.solve_with(["one", "two", "three", "four", "five", "six"])
 
-    def test_solution_failure_unmapped_region(self):
+    async def test_solution_failure_unmapped_region(self):
         """A ValueError is raised if there is any submission of Region 0"""
         with self.assertRaises(ValueError):
             # "test" is the invalid input for this puzzle; place in the middle to triangulate
-            self.puzzle.solve_with(["something", "rabbit", "reservoir", "test", "rebuttal", "butter", "abattoir"])
+            await self.puzzle.solve_with(["something", "rabbit", "reservoir", "test", "rebuttal", "butter", "abattoir"])
 
-    def test_solution_failure_overlapping_regions(self):
+    async def test_solution_failure_overlapping_regions(self):
         """A ValueError is raised if there are multiple submissions from the same region"""
         with self.assertRaises(ValueError):
             # "rabbit" and "sabbath" overlap regions in an otherwise complete set
-            self.puzzle.solve_with(["tester", "something", "rabbit", "sabbath", "rebuttal", "butter", "abattoir"])
+            await self.puzzle.solve_with(["tester", "something", "rabbit", "sabbath", "rebuttal", "butter", "abattoir"])
