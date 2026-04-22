3475d64f0b7aedd1efd9954d664cee8b2969c63b
Jim DeLois <jimdelois@users.noreply.github.com>
2026-04-22 15:32:06 -0400

    Boolean Criteria and Fluent Interface

--- src/lib/Puzzle/Config.py
+++ src/lib/Puzzle/Config.py
@@ -1,9 +1,21 @@
 import src.lib.Puzzle.Criteria.Common as Common
 import src.lib.Puzzle.Criteria.Length as Length
+from src.lib.Puzzle.Criteria import Not
 
-# TODO: Stop instantiating the Criteria here. Instead, define the class and params here, but only instantiate them when loading the puzzle
-#   "2026-03-25": [(Common.HasDoubleLetters,None), (Common.EndsWithLetter,{"letter":"r"}), (Length.AtLeastLength,{"length":8})],
 puzzle_config = {
-    "2026-03-27": [Common.EndsWithLetter("r"), Length.AtLeastLength(8), Common.HasDoubleLetters()],
-    "2026-03-23": [Common.StartsWithLetter("b"), Common.EndsWithLetter("t"), Length.ExactLength(7)],
+    "2026-04-22": [
+        Common.StartsWithLetter("r"),
+        Not(Length.ExactLength(6)).Labeled("Isn't 6 letters"),
+        Common.HasDoubleLetters().And(Common.EndsWithLetter("s"))
+    ],
+    "2026-03-27": [
+        Common.EndsWithLetter("r"),
+        Length.AtLeastLength(8),
+        Common.HasDoubleLetters()
+    ],
+    "2026-03-23": [
+        Common.StartsWithLetter("b"),
+        Common.EndsWithLetter("t"),
+        Length.ExactLength(7)
+    ],
 }
