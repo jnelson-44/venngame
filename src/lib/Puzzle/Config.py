c85fce06dbdf1dc7c435857f29770c055e5b25a6
Jim DeLois <jimdelois@users.noreply.github.com>
2026-04-23 01:28:19 -0400

    Restoring Previous 'Current' Puzzle

--- src/lib/Puzzle/Config.py
+++ src/lib/Puzzle/Config.py
@@ -3,15 +3,15 @@ import src.lib.Puzzle.Criteria.Length as Length
 from src.lib.Puzzle.Criteria import Not
 
 puzzle_config = {
-    "2026-04-22": [
-        Common.StartsWithLetter("r"),
-        Not(Length.ExactLength(6)).Labeled("Isn't 6 letters"),
-        Common.HasDoubleLetters().And(Common.EndsWithLetter("s"))
-    ],
     "2026-03-27": [
         Common.EndsWithLetter("r"),
         Length.AtLeastLength(8),
         Common.HasDoubleLetters()
+    ],
+    "2026-03-26": [
+        Common.StartsWithLetter("r"),
+        Not(Length.ExactLength(6)).Labeled("Isn't 6 letters"),
+        Common.HasDoubleLetters().And(Common.EndsWithLetter("s"))
     ],
     "2026-03-23": [
         Common.StartsWithLetter("b"),
